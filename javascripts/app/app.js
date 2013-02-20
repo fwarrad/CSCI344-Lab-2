279182f6d4fa6bd1a8ccfe176bd3d111ec2fc6e2
Fareed Warrad <fwarrad@unca.edu>
2013-02-19 22:31:24 -0500

    finished javascripts/app/app.css. final commit.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,39 +1,28 @@
-var main = function() {
-  
-  var twitter = new ctwitter.CTwitter();
-   
-      // ctwitter funtion start
-      twitter.stream("statuses/filter", { lang:"en", track:["galaxy s3", "htc m7"] }, function (stream) {
-         stream.on("data", function (tweet) {
+var $ = window.$;
+var ctwitter = window.ctwitter; 
+var console = window.console; 
+var main = function () {
+        "use strict";
+        var twitter = new ctwitter.CTwitter();
+        // ctwitter funtion start
+        twitter.stream("statuses/filter", { lang: "en", track: ["galaxy s3", "htc m7"] }, function (stream) {
+            stream.on("data", function (tweet) {
 
             //feeds out to html file
-            $("#twitter").append("<li>"+tweet.text+"</li>");
-            $("#twitter-images").append("<img src='"+tweet.profile_image_url+"' />");
-           
-            // prints 10 tweets at a time
-            if($("li").length > 10) {
-               
-              $("li:first").remove();
-            }
-            
-            // prints 10 user images at a time
-            if($("twitter-images").length > 10) {
-               
-              $("twitter-images:first").remove();
-            }
-            
-            // feeds out to console. 
-            console.log(tweet.text);
-            
-            // swaps tweets for images and vice versa
-            $("#twitter").delay(7000).fadeOut(1000);
-            $("#twitter-images").delay(8000).fadeIn(1000).delay(2000).fadeOut(800);
-            $("#twitter").delay(3500).fadeIn(500);     
-         
-         }); 
-      });
+                $("#twitter").delay(800000).fadeIn(900000).append("<ul>" + tweet.text + " </ul>");
+            // Prints 9 tweets at a time
+                if ($("ul").length > 9) {
+                    $("ul:first").fadeIn('slow');
+                    $("ul:first").remove();
+                    $("ul:first").fadeOut(500);
+                }
 
-}
+            // feeds out to console. 
+                console.log(tweet.text);            
+            }); 
+        });
+
+    }
 
 
 $(document).ready(main);
